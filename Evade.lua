8c69d3b485de24472daf1a9c1e817bbcde25b11b
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-06-08 13:26:01 +0700

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -24,7 +24,7 @@ local EvadeSector = Evade:AddLeftGroupbox("All Feature")
 
 --// because silder does not detect dotted values 
 
-EvadeSector:AddToggle("MyToggle",{Text='Full Bright',Default=false,function(Value)
+EvadeSector:AddToggle("MyToggle",{Text='FullBright',Default=false,function(Value)
 if Value then
    	Game.Lighting.Brightness = 4
 	Game.Lighting.FogEnd = 100000
@@ -37,7 +37,9 @@ Game.Lighting.Brightness = 0.5
     Game.Lighting.ClockTime = 12
 end
 end})
-
+if Settings.NoCameraShake then
+            localplayer.PlayerScripts.CameraShake.Value = CFrame.new(0,0,0) * CFrame.new(0,0,0)
+end
 EvadeSector:AddToggle("MyToggle",{Text='No Camera Shake',Default=false,function(State)
     Settings.NoCameraShake = State
 end})
@@ -59,7 +61,7 @@ EvadeSector:AddToggle("MyToggle",{Text="Enable WalkSpeed Of Nextbots",Default=fa
 		game.Players.LocalPlayers.Character.Humanoid.WalkSpeed = 16
 	end
 end})
-EvadeSector:AddToggle("MyToggle",{Text="Enable Field Of View Player",Default=false,Callback=function(v1)
+EvadeSector:AddToggle("MyToggle",{Text="Enable Added Zoom",Default=false,Callback=function(v1)
 	if v1 then
 		workspace.CurrentCamera.FieldOfView = 120
 	else
@@ -83,185 +85,13 @@ EvadeSector:AddButton({
 				
 			end    
 		})
---local TicketsLabelC5 = FarmStats:AddLabel('Total Tickets:'..localplayer:GetAttribute('Tickets'))
-
-local FindAI = function()
-    for _,v in pairs(WorkspacePlayers:GetChildren()) do
-        if not Players:FindFirstChild(v.Name) then
-            return v
-        end
-    end
-end
-
-
-local GetDownedPlr = function()
-    for i,v in pairs(WorkspacePlayers:GetChildren()) do
-        if v:GetAttribute("Downed") then
-            return v
-        end
-    end
-end
-
---Shitty Auto farm 🥶💀🤡💀🤡💀🤡
-local revive = function()
-    local downedplr = GetDownedPlr()
-    if downedplr ~= nil and downedplr:FindFirstChild('HumanoidRootPart') then
-        task.spawn(function()
-            while task.wait() do
-                if localplayer.Character then
-                    workspace.Game.Settings:SetAttribute("ReviveTime", 2.2)
-                    localplayer.Character:FindFirstChild('HumanoidRootPart').CFrame = CFrame.new(downedplr:FindFirstChild('HumanoidRootPart').Position.X, downedplr:FindFirstChild('HumanoidRootPart').Position.Y + 3, downedplr:FindFirstChild('HumanoidRootPart').Position.Z)
-                    task.wait()
-                    game:GetService("ReplicatedStorage").Events.Revive.RevivePlayer:FireServer(tostring(downedplr), false)
-                    task.wait(4.5)
-                    game:GetService("ReplicatedStorage").Events.Revive.RevivePlayer:FireServer(tostring(downedplr), true)
-                    game:GetService("ReplicatedStorage").Events.Revive.RevivePlayer:FireServer(tostring(downedplr), true)
-                    game:GetService("ReplicatedStorage").Events.Revive.RevivePlayer:FireServer(tostring(downedplr), true)
-                    break
-                end
-            end
-        end)
-    end
-end
-
---Kiriot
-
-
-local old
-old = hookmetamethod(game,"__namecall",newcclosure(function(self,...)
-    local Args = {...}
-    local method = getnamecallmethod()
-    if tostring(self) == 'Communicator' and method == "InvokeServer" and Args[1] == "update" then
-        return Settings.Speed, Settings.Jump 
-    end
-    return old(self,...)
-end))
-
-local formatNumber = (function(value) -- //Credits: https://devforum.roblox.com/t/formatting-a-currency-label-to-include-commas/413670/3
-	value = tostring(value)
-	return value:reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
-end)
-
-function Format(Int) -- // Credits: https://devforum.roblox.com/t/converting-secs-to-hsec/146352
-	return string.format("%02i", Int)
-end
-
-function convertToHMS(Seconds)
-	local Minutes = (Seconds - Seconds%60)/60
-	Seconds = Seconds - Minutes*60
-	local Hours = (Minutes - Minutes%60)/60
-	Minutes = Minutes - Hours*60
-	return Format(Hours).."H "..Format(Minutes).."M "..Format(Seconds)..'S'
-end
-
-task.spawn(function()
-    while task.wait(1) do
-        --if Settings.TicketFarm then
-            --Settings.stats.TicketFarm.duration += 1
-       -- end
-        if Settings.moneyfarm then
-            Settings.stats.TokenFarm.duration += 1
-        end 
-    end
-end)
-
---local gettickets = localplayer:GetAttribute('Tickets')
-local GetTokens = localplayer:GetAttribute('Tokens')
-
---localplayer:GetAttributeChangedSignal('Tickets'):Connect(function()
-   -- local tickets = tostring(gettickets - localplayer:GetAttribute('Tickets'))
-    --local cleanvalue = string.split(tickets, "-")
-    --Settings.stats.TicketFarm.earned = cleanvalue[2]
---end)
-
-localplayer:GetAttributeChangedSignal('Tokens'):Connect(function()
-    local tokens = tostring(GetTokens - localplayer:GetAttribute('Tokens'))
-    local cleanvalue = string.split(tokens, "-")
-    print(cleanvalue[2])
-    Settings.stats.TokenFarm.earned = cleanvalue[2]
-end)
-
-localplayer:GetAttributeChangedSignal('Tokens'):Connect(function()
-    local tokens = tostring(GetTokens - localplayer:GetAttribute('Tokens'))
-    local cleanvalue = string.split(tokens, "-")
-    print(cleanvalue[2])
-    Settings.stats.TokenFarm.earned = cleanvalue[2]
-end)
-
-task.spawn(function()
-    while task.wait() do
-        if Settings.TicketFarm then
-            TypeLabelC5:Set('Ticket Farm')
-            DurationLabelC5:Set('Duration:'..convertToHMS(Settings.stats.TicketFarm.duration))
-            EarnedLabelC5:Set('Earned:'.. formatNumber(Settings.stats.TicketFarm.earned))
-            TicketsLabelC5:Set('Total Tickets: '..localplayer:GetAttribute('Tickets'))
-
-            if game.Players.LocalPlayer:GetAttribute('InMenu') ~= true and localplayer:GetAttribute('Dead') ~= true then
-                for i,v in pairs(game:GetService("Workspace").Game.Effects.Tickets:GetChildren()) do
-                    localplayer.Character.HumanoidRootPart.CFrame = CFrame.new(v:WaitForChild('HumanoidRootPart').Position)
-                end
-            else
-                task.wait(2)
-                game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
-            end
-            if localplayer.Character and localplayer.Character:GetAttribute("Downed") then
-                game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
-                task.wait(2)
-            end
-        end
-    end
-end)
-
-
-task.spawn(function()
-    while task.wait() do
-        if Settings.AutoRespawn then
-             if localplayer.Character and localplayer.Character:GetAttribute("Downed") then
-                game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
-             end
-        end
-
-        if Settings.NoCameraShake then
-            localplayer.PlayerScripts.CameraShake.Value = CFrame.new(0,0,0) * CFrame.new(0,0,0)
-        end
-        if Settings.moneyfarm then
-            TypeLabelC5:Set('Money Farm')
-            DurationLabelC5:Set('Duration:'..convertToHMS(Settings.stats.TokenFarm.duration))
-            EarnedLabelC5:Set('Earned:'.. formatNumber(Settings.stats.TokenFarm.earned))
-            TicketsLabelC5:Set('Total Tokens: '..formatNumber(localplayer:GetAttribute('Tokens')))
-            
-            if localplayer:GetAttribute("InMenu") and localplayer:GetAttribute("Dead") ~= true then
-                game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
-            end
-            if localplayer.Character and localplayer.Character:GetAttribute("Downed") then
-                game:GetService("ReplicatedStorage").Events.Respawn:FireServer()
-                task.wait(3)
-            else
-                revive()
-                task.wait(1)
-            end
-
-        end
-        if Settings.moneyfarm == false and Settings.afkfarm and localplayer.Character:FindFirstChild('HumanoidRootPart') ~= nil then
-            localplayer.Character:FindFirstChild('HumanoidRootPart').CFrame = CFrame.new(6007, 7005, 8005)
-        end
-    end
-end)
-
---Infinite yield's Anti afk
-local GC = getconnections or get_signal_cons
-	if GC then
-		for i,v in pairs(GC(localplayer.Idled)) do
-			if v["Disable"] then
-				v["Disable"](v)
-			elseif v["Disconnect"] then
-				v["Disconnect"](v)
-			end
-		end
-	else
-		localplayer.Idled:Connect(function()
-			local VirtualUser = game:GetService("VirtualUser")
-			VirtualUser:CaptureController()
-			VirtualUser:ClickButton2(Vector2.new())
-		end)
-end
+EvadeSector:AddButton({
+			Text = "Day",
+			Func = function()
+			Game.Lighting.ClockTime = 12
+		end})
+EvadeSector:AddButton({
+			Text = "Night",
+			Func = function()
+			Game.Lighting.ClockTime = 24
+		end})
